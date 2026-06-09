bbee7505572a3bb8e08f6b40f9618f5da4712712
govindsharma010 <gs17112015@gmail.com>
2026-06-09 12:59:25 +0530

    Add Welcome email on user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
 
 
 /**
@@ -34,6 +35,8 @@ async function userRegisterController(req, res){
     },
     token
    })
+
+    await emailService.sendRegistrationEmail(user.email, user.name);
 }
 
 
@@ -72,6 +75,8 @@ async function userLoginController(req, res){
         },
         token
     })
+
+   
 }
 
 
